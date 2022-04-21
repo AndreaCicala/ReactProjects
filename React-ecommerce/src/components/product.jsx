bde353a5db1887abe3b7f3b18af0fdf1b3bb47a5
Andrea Cicala <andreacicala.93@gmail.com>
2022-04-21 14:15:18 +0200

    update

--- React-ecommerce/src/components/product.jsx
+++
@@ -1,9 +0,0 @@
-export const Product = (props) => {
-  return (
-    <article>
-      <img className="image" src={props.image} alt="Fai finta che la vedi" />
-      <h4>{props.title}</h4>
-      <p>{props.price}</p>
-    </article>
-  );
-};
